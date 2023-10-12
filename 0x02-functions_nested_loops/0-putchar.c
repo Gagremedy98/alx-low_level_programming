846580ce5de4df633280f0e188c69506c1e1e79b
Odezime Oghenevwogaga <odezimeo@gmail.com>
2023-10-12 08:53:03 -0700

    add

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * main -Entry point
- * Return always
+ * Return: always 0
  */
 
 int main(void)
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,3 @@
-/** 
- * list of headers
- */
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
